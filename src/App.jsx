67b9acf533f48304ec3f1e59714bb9d5795b1d9d
Surya Pratap Singh <72143906+surya123-ctrl@users.noreply.github.com>
2023-01-25 19:04:42 +0530

    Added dark mode in about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import About from "./components/About";
 const App = () => {
   return (
     <>
       <Navbar title="TextUtils" about="About TextUtils" />
       <div className="container my-3">
         <TextForm heading="Enter the text to analyze" />
+        {/* <About /> */}
       </div>
     </>
   );
--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 
 const TextForm = (props) => {
-  const [text, setText] = useState("Enter text here");
+  const [text, setText] = useState("");
   const handleUpperCaseCLick = () => {
     let tempText = text.toUpperCase();
+    setText(tempText);
+  };
+  const handleLowerCaseCLick = () => {
+    let tempText = text.toLowerCase();
     setText(tempText);
   };
   const handleOnChange = (event) => {
@@ -11,19 +15,36 @@ const TextForm = (props) => {
   };
   return (
     <>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          value={text}
-          onChange={handleOnChange}
-          id="myBox"
-          rows="8"
-        ></textarea>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUpperCaseCLick}>
+          UpperCase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleLowerCaseCLick}>
+          LowerCase
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={handleUpperCaseCLick}>
-        UpperCase
-      </button>
+      <div className="container my-3">
+        <h2>Your text summary</h2>
+        <p>
+          <b>{text.split(" ").length}</b> words and <b>{text.length}</b>{" "}
+          characters
+        </p>
+        <p>
+          <b>{0.008 * text.split(" ").length}</b> Minutes read
+        </p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
     </>
   );
 };
